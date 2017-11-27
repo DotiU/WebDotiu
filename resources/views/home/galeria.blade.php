2c049ca5c6cea838ecd4b4a7fafb560488ace0e0
JoseArmando1008 <mandoinuzuka@gmail.com>
2017-11-26 21:56:39 -0600

    Galeria campaña

--- resources/views/home/galeria.blade.php
+++ resources/views/home/galeria.blade.php
@@ -71,4 +71,89 @@
       </div>
       
     </section>
+    
+    
+<div class="container">  
+     
+     <div class="galeria hvr-back-pulse">
+              <h2>Campaña</h2>
+          </div>
+
+<div style="height:20px"></div>
+          
+      <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+		  <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+			 <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+			 <li data-target="#carousel-example-generic" data-slide-to="5"></li>
+			 <li data-target="#carousel-example-generic" data-slide-to="6"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="img/img1.png" class="img-responsive" alt="Imagen responsive" width="100%">
+      <div class="carousel-caption">
+
+      </div>
+
+	  </div>
+    <div class="item">
+      <img src="img/img2.png"class="img-responsive" alt="Imagen responsive" width="100%">
+      <div class="carousel-caption">
+
+      </div>
+    </div>
+
+    <div class="item">
+      <img src="img/img3.png" class="img-responsive" alt="Imagen responsive" width="100%">
+      <div class="carousel-caption">
+
+      </div>
+			</div>
+
+			<div class="item">
+				<img src="img/img4.png" class="img-responsive" alt="Imagen responsive" width="100%">
+				<div class="carousel-caption">
+
+				</div>
+    </div>
+		<div class="item">
+			<img src="img/img5.png" class="img-responsive" alt="Imagen responsive" width="100%">
+			<div class="carousel-caption">
+
+			</div>
+	</div>
+	
+	<div class="item">
+			<img src="img/img6.png" class="img-responsive" alt="Imagen responsive" width="100%">
+			<div class="carousel-caption">
+
+			</div>
+	</div>
+	
+	<div class="item">
+			<img src="img/img7.png" class="img-responsive" alt="Imagen responsive" width="100%">
+			<div class="carousel-caption">
+
+			</div>
+	</div>
+
+</div>
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+</div>
 @stop
